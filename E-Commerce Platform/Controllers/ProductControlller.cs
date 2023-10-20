aed1399d1595d80e5b5086d6ac69c75ceb0afb6a
Knyaz <qnyazface@mail.ru>
2023-10-20 19:04:43 +0400

    ProductController and ProductDetailsViewModel were added.

--- E-Commerce Platform/Controllers/ProductControlller.cs
+++ E-Commerce Platform/Controllers/ProductControlller.cs
@@ -1,4 +1,7 @@
-﻿using E_Commerce_Platform.DataBase;
+﻿using E_Commerce_Platform.Contracts;
+using E_Commerce_Platform.DataBase;
+using E_Commerce_Platform.Services.Abstracts;
+using E_Commerce_Platform.ViewModels.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce_Platform.Controllers;
@@ -8,5 +11,42 @@ namespace E_Commerce_Platform.Controllers;
 public class ProductControlller : Controller
 {
     private readonly ECommerceDBContext _dbContext;
+    private readonly IFileService _fileService;
+
+    public ProductControlller(ECommerceDBContext dbContext, IFileService fileService)
+    {
+        _dbContext = dbContext;
+        _fileService = fileService;
+    }
+
+    [HttpGet("{id}/details")]
+    public IActionResult GetDetails(int id)
+    {
+        var product = _dbContext.Products.SingleOrDefault(p => p.Id == id);
+
+        if (product is null)
+        {
+            return RedirectToAction("ErrorPage", "Auth");
+        }
+
+        var model = new ProductDetailsViewModel
+        {
+            ProductId = product.Id,
+            Name = product.Name,
+            Description = product.Description,
+            Price = product.Price,
+
+            Categories = _dbContext.CategoryProducts
+                .Where(cp => cp.ProductId == product.Id)
+                .Select(cp => cp.Category.Name)
+                .ToList(),
+            ImageUrl = _fileService
+                 .GetStaticFilesUrl(CustomUploadDirectories.Products, product.PhysicalImageName)
+
+
+        };
+
+        return Json(model);
+    }
 
 }
---
+++ E-Commerce Platform/ViewModels/User/ProductDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Commerce_Platform.ViewModels.User;
+
+public class ProductDetailsViewModel
+{
+
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public List<string> Categories { get; set; }
+    public string ImageUrl { get; set; }
+}
