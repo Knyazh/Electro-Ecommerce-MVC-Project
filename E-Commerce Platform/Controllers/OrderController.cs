e25f188b5b7e0e1b05cf830ded83ee55f0c4dd5b
Knyaz <qnyazface@mail.ru>
2023-10-21 17:17:00 +0400

    IOrderService add and Interface implement, OrderController done.

--- E-Commerce Platform/Controllers/OrderController.cs
+++ E-Commerce Platform/Controllers/OrderController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using E_Commerce_Platform.Contracts;
+using E_Commerce_Platform.DataBase;
+using E_Commerce_Platform.DataBase.Models;
+using E_Commerce_Platform.Services.Abstracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce_Platform.Controllers;
@@ -8,5 +12,42 @@ namespace E_Commerce_Platform.Controllers;
 
 public class OrderController : Controller
 {
+    private readonly INotificationService _nofitificationService;
+
+    public OrderController(INotificationService nofitificationService)
+    {
+        _nofitificationService = nofitificationService;
+    }
+
+    [HttpPost("place-order")]
+    public IActionResult PlaceOrder(
+       [FromServices] ECommerceDBContext dbContext,
+       [FromServices] IUserService userService,
+       [FromServices] IOrderService orderService,
+       [FromServices] IFileService fileService)
+       
+    {
+        var order = new Order
+        {
+            Status = OrderStatus.Created,
+            TrackingCode = orderService.GenerateTrackingCode(),
+            UserId = userService.CurrentUser.Id,
+        };
+
+     
+        decimal total = 0;
+        var orderItems = new List<OrderItem>();
+
+      
+
+        order.OrderItems = orderItems;
+
+        _nofitificationService.SendOrderNotification(order);
+
+        dbContext.Orders.Add(order);
+        dbContext.SaveChanges();
+
+        return RedirectToAction("orders", "account");
+    }
 
 }
---
+++ E-Commerce Platform/Services/Concretes/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using E_Commerce_Platform.DataBase;
+using E_Commerce_Platform.Services.Abstracts;
+
+namespace E_Commerce_Platform.Services.Concretes
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ECommerceDBContext _dbContext;
+        private readonly Random _random;
+        private const string TRACKINGCODE_PREFIX = "OR";
+
+        public OrderService(ECommerceDBContext dpContext)
+        {
+            _dbContext = dpContext;
+            _random = new Random();
+        }
+
+        public string GenerateTrackingCode()
+        {
+            string trackingCode;
+
+            do
+            {
+                int numberPart = _random.Next(100000, 1000000);
+                trackingCode = TRACKINGCODE_PREFIX + numberPart;
+
+            } while (_dbContext.Orders.Any(o => o.TrackingCode == trackingCode));
+
+
+            return trackingCode;
+        }
+    }
+}
