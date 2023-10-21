c3b3d5c290e529848a230b575779ac8ba4ce11eb
Knyaz <qnyazface@mail.ru>
2023-10-21 16:49:38 +0400

    AlerMessagehub aded.

---
+++ E-Commerce Platform/Hubs/AlertMessageHub.cs
@@ -0,0 +1,43 @@
+﻿using E_Commerce_Platform.Services.Abstracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace E_Commerce_Platform.Hubs;
+
+[Authorize]
+public class AlertMessageHub : Hub
+{
+    private readonly ILogger<AlertMessageHub> _logger;
+    private readonly IAlertMessageService _alertMessageService;
+    private readonly IUserService _userService;
+
+    public AlertMessageHub(
+        ILogger<AlertMessageHub> logger,
+        IAlertMessageService alertMessageService,
+        IUserService userService)
+    {
+        _logger = logger;
+        _alertMessageService = alertMessageService;
+        _userService = userService;
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        _alertMessageService.AddConnectionId(_userService.CurrentUser, Context.ConnectionId);
+
+        _logger.LogInformation($"New connection established : {Context.ConnectionId}");
+
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception exception)
+    {
+        _alertMessageService.RemoveConnectionId(_userService.CurrentUser, Context.ConnectionId);
+
+        _logger.LogInformation($"Connection finished established : {Context.ConnectionId}");
+
+        return base.OnDisconnectedAsync(exception);
+    }
+
+
+}
--- E-Commerce Platform/Services/Concretes/NotificationService.cs
+++ E-Commerce Platform/Services/Concretes/NotificationService.cs
@@ -1,10 +1,36 @@
-﻿using E_Commerce_Platform.DataBase.Models;
+﻿using E_Commerce_Platform.Contracts;
+using E_Commerce_Platform.DataBase;
+using E_Commerce_Platform.DataBase.Models;
+using E_Commerce_Platform.Hubs;
 using E_Commerce_Platform.Services.Abstracts;
+using Microsoft.AspNetCore.SignalR;
+using System.Text;
 
 namespace E_Commerce_Platform.Services.Concretes;
 
 public class NotificationService : INotificationService
 {
+
+    private readonly IEmailService _emailService;
+    private readonly ECommerceDBContext _dbContext;
+    private readonly IUserService _userService;
+    private readonly IHubContext<AlertMessageHub> _hubContext;
+    private readonly IAlertMessageService _aletMessageService;
+
+    public NotificationService(
+        IEmailService emailService,
+        ECommerceDBContext dbContext, 
+        IUserService userService,
+        IHubContext<AlertMessageHub> hubContext,
+        IAlertMessageService aletMessageService)
+    {
+        _emailService = emailService;
+        _dbContext = dbContext;
+        _userService = userService;
+        _hubContext = hubContext;
+        _aletMessageService = aletMessageService;
+    }
+
     public void CreateAndPushAlerMessage(List<User> recievers, string title, string content)
     {
         throw new NotImplementedException();
@@ -25,9 +51,53 @@ public class NotificationService : INotificationService
         throw new NotImplementedException();
     }
 
+    private string PrepareAlertMessageContent(Order order, string template)
+    {
+        var templayeBuilder = new StringBuilder(template)
+           .Replace("{order_number}", order.TrackingCode);
+
+        return templayeBuilder.ToString();
+    }
+    private string PrepareEmailMessageContent(Order order, string template)
+    {
+        var templayeBuilder = new StringBuilder(template)
+            .Replace("{order_number}", order.TrackingCode)
+            .Replace("{firstName}", order.User.Name)
+            .Replace("{lastName}", order.User.LastName);
+
+        return templayeBuilder.ToString();
+    }
+
     public void SendOrderNotification(Order order)
     {
-        throw new NotImplementedException();
+        switch (order.Status)
+        {
+            case OrderStatus.Created:
+                SendOrderCreatedNotification(order);
+                break;
+            case OrderStatus.Approved:
+                SendOrderApprovedNotification(order);
+                break;
+            case OrderStatus.Rejected:
+                SendOrderRejectedNotification(order);
+                break;
+            case OrderStatus.Sent:
+                SendOrderSentNotification(order);
+                break;
+            case OrderStatus.Completed:
+                SendOrderCompletedNotification(order);
+                break;
+            default:
+                throw new NotificationNotImplementedException();
+        }
+    }
+
+
+    public void SendOrderCreatedNotification(Order order)
+    {
+        var emailMessageContent = PrepareAlertMessageContent(order, AlertMessageTemplates.Order.CREATED);
+        var staffMembers = _userService.GetAllStaffMembers();
+        CreateAndPustOrderAlertMessage(staffMembers, emailMessageContent);
     }
 
     public void SendOrderRejectedNotification(Order order)
